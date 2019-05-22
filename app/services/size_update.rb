4640ae8a01ad706b676c5250ef743124f7ea5cd8
nevillealee <nevillealee@gmail.com>
2019-05-22 15:07:43 -0700

    realtime updates in progress

--- app/services/size_update.rb
+++ app/services/size_update.rb
@@ -12,6 +12,7 @@ class SizeUpdate
   end
 
   def run
+    stream_pre_update
     Resque.logger = Logger.new("#{Rails.root}/log/order_size_update.log", 5, 10024000)
     Resque.logger.level = Logger::INFO
     Resque.logger.info "PARAMS IN ORDERSIZEUPDATE #{@form_data.inspect}"
@@ -43,6 +44,7 @@ class SizeUpdate
       sub.save!
       Resque.logger.info "New Subscription sizes: #{sub.sizes}"
     else
+      stream_failure
       exit
       Resque.logger.error "SUB ##{sub.id} WAS NOT UPDATED IN DB OR RECHARGE!!!"
     end
@@ -64,9 +66,10 @@ class SizeUpdate
       my_order.save!
       Resque.logger.info("New Order sizes: #{my_order.sizes(sub_id)}")
       puts "Order size update Done"
-      stream_complete_update
+      stream_complete_update(res2)
     else
       Resque.logger.error "ORDER ##{my_order.id} WAS NOT UPDATED IN DB OR RECHARGE!!!"
+      stream_failure(res2)
     end
   end
 
@@ -82,16 +85,25 @@ class SizeUpdate
 
   def stream_pre_update
     ActionCable.server.broadcast "notifications:TEST", {html:
-      "<div class='alert alert-warning alert-block text-center'>
+      "<div class='alert alert-primary alert-block text-center'>
           Sending update sizes request to Recharge API....
       </div>"
       }
   end
 
-  def stream_complete_update
+  def stream_complete_update(response)
       ActionCable.server.broadcast "notifications:TEST", {html:
     "<div class='alert alert-success alert-block text-center'>
-        Update complete!
+       #{response}
+     </div>"
+      }
+  end
+
+  def stream_failure(res2)
+      ActionCable.server.broadcast "notifications:TEST", {html:
+    "<div class='alert alert-danger alert-block text-center'>
+        Recharge API Error: #{res2["errors"]}
+        Link to failed order <a href='/customer/orders/#{@form_data['order_id']}'>here</a>
      </div>"
       }
   end
