599755d0f818c7e75fe60d29fdde7bda0ecfe6a8
nevillealee <nevillealee@gmail.com>
2019-05-29 16:04:52 -0700

    fixes partial email search query error

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,21 +10,29 @@ class Customer < ApplicationRecord
   # @param query [String] search parameter
   # @return [Array<ActiveRecord::Relation>] matching or all customers in asc order
   def self.search(query)
-    if query
+    if query.present?
       my_query = query.strip
-      if /^[a-zA-Z ]*$/.match(my_query)
+      puts "made it inside condition"
+      puts my_query
+      if /^[a-zA-Z .]*$/.match(my_query) || /@/.match(my_query) || /^([0-9]*[a-zA-Z][a-zA-Z0-9_]*)$/.match(my_query)
         queries = my_query.split(' ')
+        puts "multi string query"
         if queries.size > 1
           joins(:orders).where(:orders => {:status => "QUEUED"}).where("customers.first_name ilike ? AND customers.last_name ilike ?", "%#{queries[0]}%", "%#{queries[1]}%").order(first_name: :asc)
         else
+          puts "email query"
           joins(:orders).where(:orders => {:status => "QUEUED"}).where("customers.first_name ilike ? OR customers.last_name ilike ? OR customers.email ilike ?",
-            "%#{queries[0]}%", "%#{queries[0]}%", "%#{queries[0]}%"
-          ).order(first_name: :asc).uniq
+            "#{queries[0]}%", "#{queries[0]}%", "%#{queries[0]}%"
+          ).order(first_name: :asc)
         end
       elsif /^[\d\s]+$/.match(my_query)
-        joins(:orders).where(:orders => {:status => "QUEUED"}).where("customers.shopify_customer_id = ? OR id = ?", "#{my_query}", "#{my_query}").order(first_name: :asc).uniq
+        puts "number query"
+        joins(:orders).where(:orders => {:status => "QUEUED"}).where("customers.shopify_customer_id = ? OR orders.id = ?", "#{my_query}", "#{my_query}").order(first_name: :asc).uniq
+      else
+        joins(:orders).where(:orders => {:status => "QUEUED"}).order(first_name: :asc).uniq
       end
     else
+      puts "non string or number query"
       joins(:orders).where(:orders => {:status => "QUEUED"}).order(first_name: :asc).uniq
     end
   end
